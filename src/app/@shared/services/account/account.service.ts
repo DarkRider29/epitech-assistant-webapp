f3ec79b33a10ac62d68e4e3257ef348321a1cdd3
jibs <jean-baptiste.detroyes@epitech.eu>
2019-03-06 22:55:49 +0100

    [TECH] Fix

--- src/app/@shared/services/account/account.service.ts
+++ src/app/@shared/services/account/account.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {OfficeService} from '../api/office365/office.service';
-import {type} from 'os';
 
 @Injectable({
   providedIn: 'root'
