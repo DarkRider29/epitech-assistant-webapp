7178ab3c26c487bb8459bfc6e6ba6a10e6cd7805
jibs <jean-baptiste.detroyes@epitech.eu>
2019-04-06 16:12:37 +0200

    [TECH] env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,18 +4,18 @@
 
 export const environment = {
   production: false,
-  azureApp: 'da5c5f66-b435-4ba7-bc3c-3fae0c53bd58',
-  googleId: '493942636949-jq758v4jcd8g24vetmatvscebqqkl878.apps.googleusercontent.com',
-  googleSecret: 'd8XvQx8ogHw_RMEn6hH1PUBy',
-  amazonId: 'amzn1.application-oa2-client.fa7c6bafd1454d05822fa29588da812c',
-  epitechAPI: 'https://assistant.detroyes.fr/intra/',
+  azureApp: '',
+  googleId: '',
+  googleSecret: '',
+  amazonId: '',
+  epitechAPI: '',
   firebaseConfig: {
-    apiKey: "AIzaSyDTEFYuQxvxGoT9DPTvzuDC1BnfdEJkD8I",
-    authDomain: "epitech-b2910.firebaseapp.com",
-    databaseURL: "https://epitech-b2910.firebaseio.com",
-    projectId: "epitech-b2910",
-    storageBucket: "epitech-b2910.appspot.com",
-    messagingSenderId: "493942636949"
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
 
